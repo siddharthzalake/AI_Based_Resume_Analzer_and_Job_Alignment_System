762e1597d8a7ae3e1d4a27765ddd0ade2e7f9280
siddharthzalake <zalakesiddharth@gmail.com>
2026-03-20 18:01:07 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:`${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
-  timeout: 60000
+  timeout: 240000
 });
 
 export default api;
